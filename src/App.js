e2064870b16d95c0eab8152a4e18993fc6eb460e
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-20 21:05:56 -0600

    se pusieron las rutas de las paginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+
+
+//--- componentes ---//
+import Ordenes from './components/paginas/Ordenes';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+import Menu from './components/paginas/Menu';
+import Sidebar from './components/ui/Sidebar'
 
 function App() {
   return (
-      <h1 className='text-base'>Hola mundo</h1>
+    
+    <div>
+
+      <div className='md:flex min-h-screen'>
+        <Sidebar />
+
+        <div className='md:w-3/5 xl:w-4/5 p-6'>
+          <Routes>
+            <Route path='/' element={ <Ordenes /> } />
+            <Route  path='/menu' element={ <Menu /> }  />
+            <Route path='/nuevo-platillo' element={ <NuevoPlatillo /> } />
+          </Routes>
+        </div>
+
+
+      </div>
+
+    </div>
+
   );
 }
 
