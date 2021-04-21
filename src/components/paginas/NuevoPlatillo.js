53f27138fc69f6723d4c2f5d9f425f76a5fadd97
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-21 14:24:40 -0600

    añadiendo Formik al formulario y mostrando mensajes de error

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFormik } from 'formik';// para validar los formularios
 
-
+import * as Yup from 'yup';
 
 const NuevoPlatillo = () => {
 
@@ -14,6 +14,22 @@ const NuevoPlatillo = () => {
             imagen: '',
             descripcion: ''
         },
+
+        //--- Validand el formulario ---//
+        validationSchema: Yup.object({
+            nombre: Yup.string()
+                        .min(3,'Los platillos deben tener al menos 3 caracteres')
+                        .required('El Nombre del platillo es obligatorio'),
+            precio: Yup.number()
+                        .min(1, 'Debes agregar un numero' )
+                        .required('El precio es obligatorio'),
+            categoria: Yup.string()
+                        .required('La Categoria es obligatoria'),
+            descripcion: Yup.string()
+                            .min(10, 'La descripcion debe de ser mas larga')
+                            .required('La descripcion es obligatoria')
+        }),
+
         onSubmit: datos =>{ // se pasa los datos del formularios 
             console.log(datos)
         }   
@@ -39,8 +55,17 @@ const NuevoPlatillo = () => {
                                 placeholder='Nombre Platillo'
                                 value={formik.values.nombre}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}//valida inmediatamente el input
                            />
                        </div>
+
+                       {/** muestra el mensaje para la validacion */}
+                       {formik.touched.nombre && formik.errors.nombre ? (
+                           <div className='bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5' role='alert'>
+                               <p className='font-bold'>Hubo un error:</p>
+                               <p>{formik.errors.nombre}</p>
+                           </div>
+                       ): null}
 
                        <div className='mb-4'>
                            <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='precio'>Precio</label>
@@ -52,9 +77,17 @@ const NuevoPlatillo = () => {
                                 min='0'
                                 value={formik.values.precio}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                            />
-
                        </div>
+
+                       {/** muestra el mensaje para la validacion */}
+                       { formik.touched.precio && formik.errors.precio ? (
+                            <div className='bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5' role='alert'>
+                                <p className='font-bold'>Hubo un errro</p>
+                                <p>{formik.errors.precio}</p>
+                            </div>
+                       ): null }
 
                        
                        <div className='mb-4'>
@@ -64,16 +97,27 @@ const NuevoPlatillo = () => {
                             id='categoria'
                             name='categoria'
                             value={formik.values.categoria}
+                            onBlur={formik.handleBlur}
                             >
                                 <option value=''> --- Seleccione ---  </option>
-                                <option value='desayuno'>  Desayuno   </option>
-                                <option value='comida'>  Comida  </option>
-                                <option value='cena'>  Seleccione   </option>
-                                <option value='bebidas'>  Bebidas   </option>
-                                <option value='postre'>  Postre   </option>
-                                <option value='ensalada'>  Ensalada   </option>
+                                <option value='desayuno'>Desayuno</option>
+                                <option value='comida'>Comida</option>
+                                <option value='bebidas'>Bebidas</option>
+                                <option value='postre'>Postre</option>
+                                <option value='ensalada'>Ensalada</option>
                            </select>
                        </div>
+
+                        {/** muestra el mensaje para la validacion */}
+                        {formik.touched.categoria && formik.errors.categoria ? (
+
+                            <div className='bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5' role='alert'>
+                                <p className='font-bold'>Hubo un error</p>
+                                <p>{formik.errors.categoria}</p>
+                            </div>
+
+                        ) : null}
+
 
                        
                        <div className='mb-4'>
@@ -84,6 +128,7 @@ const NuevoPlatillo = () => {
                                 type='file'
                                 value={formik.values.imagen}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                            />
                        </div>
 
@@ -95,10 +140,18 @@ const NuevoPlatillo = () => {
                                 placeholder='Descripcion del platillo'
                                 value={formik.values.descripcion}
                                 onChange={formik.handleChange}
-
+                                onBlur={formik.handleBlur}
                             >
                             </textarea>
                        </div>
+
+                        {/** muestra el mensaje para la validacion */}
+                        {formik.touched.descripcion && formik.errors.descripcion ? (
+                            <div className='bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5' role='alert'>
+                                <p className='font-bold'>Hubo un eror</p>
+                                <p>{formik.errors.descripcion}</p>
+                            </div>
+                        ) : null}
 
                         <input 
                             type='submit'
