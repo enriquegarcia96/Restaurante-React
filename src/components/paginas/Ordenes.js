d0f2d0f19bc2770a091829b1ebad40193558227d
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-20 21:08:08 -0600

    lleva a agregar un nuevo platillo

---
+++ src/components/paginas/Ordenes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Ordenes = () => {
+    return (
+        <>
+
+            <div className='text-3xl font-light mb-4'>Ordenes</div> 
+
+        </>    
+    )
+}
+
+export default Ordenes;
+
+
